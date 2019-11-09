34a2efb635ad71fcc277130c0f76058c239ea249
Rafal ROjek <rafalrojek90@gmail.com>
2019-11-09 11:58:57 +0100

    create a new recipe list

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,13 @@
+export class Ingredient {
+    //this is the same as speciying the constructor 
+    constructor(public name : string, public amount : number) {};
+
+    // public name : string;
+    // public amount : number;
+
+    // constructor(name : string, amount : number){
+    //     this.name = name;
+    //     this.amount = amount;
+    // }
+
+}
