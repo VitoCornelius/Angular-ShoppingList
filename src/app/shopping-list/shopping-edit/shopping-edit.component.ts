8f99259711dde459623ceba51f3ebbb94cccd0da
Rafal ROjek <rafalrojek90@gmail.com>
2019-11-10 11:43:44 +0100

    adding the new ingredient via the GUI

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Output, EventEmitter, ElementRef } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('nameInput') nameInputRef : ElementRef;
+  @ViewChild('amountInput') amountInputRef : ElementRef; //always add the element ref 
+
+  @Output('addIngredient') ingredientAdded = new EventEmitter<Ingredient>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addIngredient(){
+    const newIngredient = new Ingredient(this.nameInputRef.nativeElement.value, this.amountInputRef.nativeElement.value)
+    this.ingredientAdded.emit(newIngredient);
   }
 
 }
