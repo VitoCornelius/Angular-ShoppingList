804609e1a178cfec974c89416d7adbb1504a949b
Rafal ROjek <rafalrojek90@gmail.com>
2020-01-13 20:42:18 +0100

    populate the form with the ingredients

--- src/app/recipes/recipe-added/recipe-added.component.ts
+++ src/app/recipes/recipe-added/recipe-added.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 import { RecipeService } from '../recipe.service';
 import { Recipe } from '../recipe.model';
 
@@ -33,6 +33,7 @@ export class RecipeAddedComponent implements OnInit {
     let recipeName = '';
     let recipeImagePath = '';
     let recipeDescription = '';
+    let recipeIngredients = new FormArray([]); //default value of an empry array
 
 
     if (this.editMode) {
@@ -40,17 +41,35 @@ export class RecipeAddedComponent implements OnInit {
       recipeName = recipe.name;
       recipeImagePath = recipe.imagePath;
       recipeDescription = recipe.description;
+
+      if (recipe['ingredients']) {  //if it has the ingredients 
+        for (let ingredient of recipe.ingredients){
+          recipeIngredients.push(
+            new FormGroup(
+              {
+                'name' : new FormControl(ingredient.name),
+                'amount' : new FormControl(ingredient.amount)
+              }
+            )
+          )
+        }
+      }
     }
 
     this.recipeForm = new FormGroup({
       'name' : new FormControl(recipeName), //set the default value
       'imagePath' : new FormControl(recipeImagePath),
-      'description' : new FormControl(recipeDescription)
+      'description' : new FormControl(recipeDescription),
+      'ingredients' : recipeIngredients
     })
   }
 
   onSubmit() {
     console.log(this.recipeForm);
+  }
+
+  getControls() {
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
   }
 
 }
