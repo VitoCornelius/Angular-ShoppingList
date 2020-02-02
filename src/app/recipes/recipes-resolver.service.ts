1a3e51cfea95cd898527e4b624641098e2227ba7
Rafal ROjek <rafalrojek90@gmail.com>
2020-02-02 17:37:00 +0100

    Fix a bug in the resolver. Do not allow the page to reload if the recipes are already there

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -3,13 +3,19 @@ import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/r
 import { Recipe } from "./recipe.model";
 import { DataStorageService } from "../shared/data-storage.service";
 import { Observable } from "rxjs";
+import { RecipeService } from "./recipe.service";
 
  @Injectable({providedIn : 'root'})
 export class RecipesResolverService implements Resolve<Recipe[]> {
-    constructor(private dataStorageService : DataStorageService){}
+    constructor(private dataStorageService : DataStorageService, private recipeService : RecipeService){}
 
     resolve(route : ActivatedRouteSnapshot, state : RouterStateSnapshot){
-         return this.dataStorageService.fetchRecipes();
+        if (this.recipeService.getRecipes().length === 0){
+            return this.dataStorageService.fetchRecipes();
+        } else {
+            return;
+        }
+
     }
 
 }
